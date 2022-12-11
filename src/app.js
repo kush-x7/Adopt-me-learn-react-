f2c1292e7f303c39942264e921e1125898a6d749
kushagra <kushagratripathi7@gmail.com>
2022-12-11 12:19:13 +0530

    add: props

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h1", {}, props.animal),
+    React.createElement("h1", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div", //Element name || Tag name
+    { id: "my-id" }, //Element attribute
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        animal: "kush",
+        name: "dhr",
+        breed: "dog",
+      }),
+      React.createElement(Pet, {
+        animal: "kush",
+        name: "dhr",
+        breed: "dog",
+      }),
+      React.createElement(Pet, {
+        animal: "kush",
+        name: "dhr",
+        breed: "dog",
+      }),
+    ] //Element  CHildren
+  );
+};
+
+const getRoot = document.getElementById("root");
+const virtualRoot = ReactDOM.createRoot(getRoot);
+virtualRoot.render(React.createElement(App));
